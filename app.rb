276838df476103fcb24d65bb4e3a026f75c7523e
Sean Keith McAuley <sean@zarokeanpie.com>
2012-01-04 03:32:23 -0800

    foo

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
+require 'builder'
 require 'twilio-ruby'
 
 before do
   @client = Twilio::REST::Client.new ENV['TWILIO_ID'], ENV['TWILIO_SECRET']
 end
 
-post '/responder' do
-  Twilio::TwiML::Response.new do |r|
-    r.say "Hello Monkey"
+get '/responder' do
+  builder do |xml|
+    xml.instruct!
+    xml.Response do
+      xml.Say('Hi World')
+    end
   end
 end
